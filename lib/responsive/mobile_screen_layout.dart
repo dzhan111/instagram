b78921baaf7a3927b4eb9bc61f48167a69bc2f80
dzhan111 <zhandavid4@gmail.com>
2024-06-17 14:03:44 -0400

    post saved to firebase

--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -44,10 +44,10 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
     model.User? user = Provider.of<UserProvider>(context).getUser;
     return Scaffold(
       body: PageView(
-        children: homeScreenItems,
-        physics: NeverScrollableScrollPhysics(),
+        physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
         onPageChanged: onPageChanged,
+        children: homeScreenItems,
       ),
       bottomNavigationBar: CupertinoTabBar(
         backgroundColor: mobileBackgroundColor,
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -3,11 +3,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:instagram_flutter/screens/add_post_screen.dart';
+import 'package:instagram_flutter/screens/feed_screen.dart';
 
 const webScreenSize = 600;
 
 const homeScreenItems = [
-          Text('feed'),
+          FeedScreen(),
           Text('seach'),
           AddPostScreen(),
           Text('fedfdsed'),
